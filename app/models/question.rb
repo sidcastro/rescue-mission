d92f6bb7d7d33be3ccb291336971ecb4a31ed662
Sidney Castro <sidneycastro96@gmail.com>
2018-08-08 14:24:43 -0400

    Post a Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { minimum: 20 }
+  validates :description, presence: true, length: { minimum: 50 }
 end
